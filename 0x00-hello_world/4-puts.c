f57344bc77b1aa4c223ae50acead38460c1000ff
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-08 13:25:55 -0700

    Hello. puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * *main - Printing
+ * main - Printing
  * Return: Always 0 (Sucessful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
