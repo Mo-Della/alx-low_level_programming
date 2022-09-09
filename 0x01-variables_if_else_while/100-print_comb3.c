eac495d4b0ce2b47874bf2b309c3367f9b992456
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-09 08:41:04 -0700

    Two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit 2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
@@ -27,3 +27,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+}
