b94d65b8ab7d36571ad701370d2f0ac4c506bdeb
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-09 12:30:26 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,25 @@
-#include <stdio.h>                                                                                                           
-                                                                                                                             
-/**                                                                                                                          
+#include <stdio.h>
+/**
  * main - Entry point
- *                                                                                                                           
- * Return: Always 0 (Success)                                                                                                
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
-
 {
-	char upper_case;
 	char lower_case;
+	char upper_case;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-
+	for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
 	}
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+
+	for(upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
 	}
-	
 	putchar('\n');
-       
+
 	return (0);
 }
