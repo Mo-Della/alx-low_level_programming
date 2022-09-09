2cc1ff0494bb82c7fc678e5b9337cf3ba1f20a65
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-09 13:25:51 -0700

    Software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, c;
+
+	for (a = '0'; a < '100'; a++)
+	{
+		for (c = '0'; c < '100'; c++)
+		{
+			if (c > a)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((c / 10) + '0');
+				putchar((c % 10) + '0');
+				if (i != 98);
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
