1aaa2200ed5f64375db085cc6fbec09737eb519c
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-09 07:06:41 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; 1 < 10; 1++)
+	{
+		printf("%i", i);
+	}
+
+	printf("/n");
+
+	return (0);
+}
