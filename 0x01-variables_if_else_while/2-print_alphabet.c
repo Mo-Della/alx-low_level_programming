62de9b6979abda0e72c60c3b82288ac28fafea4a
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-09 06:08:25 -0700

    Alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char alphabeth;
+
+	for (alphabeth = 'a'; alphabeh <= 'z'; alphabeth++)
+	{
+		putchar(alphabeth);
+	}
+		putchar('n\');
+
+		retuen (0);
+}
