d809788dcaf4cc0cb160ce76aa93ab8f2756f548
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-14 12:40:18 -0700

    Mulle

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests function for positive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
