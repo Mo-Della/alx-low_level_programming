7633a14d18f075bcd7f0d085f4883c7e2412f453
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-21 07:34:57 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - checks the code
+ * @dest: variable
+ * @src: variable
+ * @n: integer
+ * Return: concatinated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; src[i] != '\0' && i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
