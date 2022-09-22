02c5aed9837a01bdb4950813904eae2ca727cd36
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-22 07:18:19 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,18 +2,21 @@
 /**
  * print_number - prints an integer
  * @n: integer
+ * Return: nothing
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n *= -1;
 	}
 
-	if ((num / 10) > 0)
+	num = n;
+
+	if ((num / 10)
 		print_number(num / 10);
 
 	_putchar((num % 10) + '0')
