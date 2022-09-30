f4fb596562d6d42afcbc0a42cd2f84b714452d29
Mo-Della <favourokhiria234@gmail.com@example.com>
2022-09-30 13:02:33 -0700

    static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * File: Main.h
- * Desc: Header file containing all the prototypes of all functions included in the static library
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
